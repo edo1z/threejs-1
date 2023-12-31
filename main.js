56f1ea6169fcc96fa40d59ffb7fa7ecb835ce6f0
edo1z <endo.yuta@logicky.com>
2023-12-31 12:56:44 +0900

    線を表示したぜえ

--- main.js
+++ main.js
@@ -12,12 +12,27 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// camera
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+// cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+// line
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+points.push(new THREE.Vector3(0, -10, 0));
+points.push(new THREE.Vector3(-10, 0, 0));
+const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(lineGeometry, lineMaterial);
+scene.add(line);
 
 function animate() {
   requestAnimationFrame(animate);
